fcc8c3d417792df17dd27456a425e68b5dc18b33
Jonathan Sexton <joncsexton@gmail.com>
2018-11-18 18:53:54 -0600

    trying to get Gulp tasks to work

--- personal-portfolio/gulpfile.js
+++ personal-portfolio/gulpfile.js
@@ -1,21 +1,21 @@
-let gulp = require("gulp");
-let cleanCSS = require("gulp-clean-css");
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
 
 // Task to minify css using package cleanCSS
 gulp.task("minify-css", () => {
   // Folder with files to minify
   return (
     gulp
-      .src("css/*.css")
+      .src("./css/**/*.css")
       .pipe(cleanCSS())
       //This method declares where minified files will be placed
       .pipe(gulp.dest("minified"))
   );
 });
-//We create a 'default' task that will run when we run `gulp` in the project
-gulp.task("default", function() {
+// We create a 'default' task that will run when we run `gulp` in the project
+gulp.task("default", function(evt) {
   // We use `gulp.watch` for Gulp to expect changes in the files to run again
-  gulp.watch("./css/*.css", function(evt) {
+  gulp.watch("./css/**/*.css", function(evt) {
     gulp.task("minify-css");
   });
 });
